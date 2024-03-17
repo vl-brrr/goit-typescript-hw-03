4b2cd926d3732150faad04341defdcadb2566c66
vl-brrr <vlada.brayko@gmail.com>
2024-03-17 15:56:10 +0200

    Done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  private tenants: Person[] = [];
+  public door: boolean = false;
+  constructor(protected key: Key) {}
+
+  comeIn(tenant: Person) {
+    if (this.door) {
+      this.tenants.push(tenant);
+      console.log("Come in ...");
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+  openDoor(key: Key): void {
+    if (key === this.key) {
+      this.door = true;
+      console.log("The door is opened.");
+    } else {
+      console.log("Can`t open the house with this key.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +53,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
